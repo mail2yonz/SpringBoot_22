afcb939b42f20ccb7b82cfb37dfe040513c8d347
Yonatan Mengesha <mail2yonz@gmail.com>
2017-08-24 17:01:29 -0400

    First Commit

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -18,9 +18,9 @@ public class HomeController {
         return "login";
     }
 
-    @RequestMapping("/admin")
-    public String admin()
+    @RequestMapping("/secure")
+    public String secure ()
     {
-        return "admin";
+        return "secure";
     }
 }
--- src/main/java/com/example/demo/SpringBoot20Application.java
+++ src/main/java/com/example/demo/SpringBoot20Application.java
@@ -4,9 +4,9 @@ package com.example.demo;
 		import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBoot19Application {
+public class SpringBoot20Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBoot19Application.class, args);
+		SpringApplication.run(SpringBoot20Application.class, args);
 	}
 }
