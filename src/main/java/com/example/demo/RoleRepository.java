a0172681d14b77d8cb70a4e2c008e976ec2e57f8
Yonatan Mengesha <mail2yonz@gmail.com>
2017-08-25 11:11:01 -0400

    First commit'

--- src/main/java/com/example/demo/RoleRepository.java
+++ src/main/java/com/example/demo/RoleRepository.java
@@ -3,4 +3,6 @@ package com.example.demo;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RoleRepository extends CrudRepository<Role,Long>{
+
+    Role findByRole(String role);
 }
--- src/main/java/com/example/demo/SecurityConfigration.java
+++ src/main/java/com/example/demo/SecurityConfigration.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,6 +12,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfigration extends WebSecurityConfigurerAdapter{
 
 @Autowired
@@ -28,7 +30,7 @@ private UserRepository userRepository;
     protected void configure(HttpSecurity http) throws Exception {
          http
                  .authorizeRequests()
-                 .antMatchers("/","/h2-console/**").permitAll()
+                 .antMatchers("/","/h2-console/**","/register").permitAll()
                  .anyRequest().authenticated()
                  .and()
                  .formLogin().loginPage("/login").permitAll()
@@ -45,6 +47,7 @@ private UserRepository userRepository;
 
     }
 
+    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception
     {
         auth.userDetailsService(userDetailsServiceBean());
